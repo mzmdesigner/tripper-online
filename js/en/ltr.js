6362fe70b874fd5961d022529a6cb5a1d602ec97
Mostafa Zakaria Mohamed <49752437+mzmdesigner@users.noreply.github.com>
2020-05-25 20:56:43 +0200

    Update ltr.js

--- js/en/ltr.js
+++ js/en/ltr.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($){
-
+    
+    // Hero Slider Settings » Home
     $('.hero-slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -10,6 +11,7 @@ jQuery(document).ready(function($){
         cssEase: 'linear',
     });
 
+    // Category Slider Settings » Home
     $('.category-slider').slick({
         infinite: true,
         slidesToShow: 4,
@@ -21,10 +23,24 @@ jQuery(document).ready(function($){
         nextArrow:"<button class='btn btn-primary btn-next'><i class='icofont-long-arrow-left'></i></button>",
     });
 
+    // Popular Trip Slider Settings » Home
+    $('.popular-trip-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        arrows: true,
+        prevArrow:"<button class='btn btn-primary btn-prev'><i class='icofont-long-arrow-right'></i></button>",
+        nextArrow:"<button class='btn btn-primary btn-next'><i class='icofont-long-arrow-left'></i></button>",
+    });
+
+    // Datepicker From » Home
     $(function() {
         $( "#datepicker1" ).datepicker();
     });
 
+    // Datepicker To » Home
     $(function() {
         $( "#datepicker2" ).datepicker();
     });
